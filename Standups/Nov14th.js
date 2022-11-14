356b1df524e7a3d594a15b98b8e9224fd556c99a
Luiz Meneghim <luiz.meneghim@hotmail.com>
2022-11-14 16:23:13 -0700

    finished standup

--- Standups/Nov14th.js
+++ Standups/Nov14th.js
@@ -16,6 +16,9 @@
 
 // From: https://www.codewars.com/kata/545a4c5a61aa4c6916000755
 
-function gimme(triplet) {
-  return triplet;
+function gimme(arr) {
+  return arr.indexOf([...arr].sort((a, b) => a - b)[1]);
 }
+
+console.log(gimme([2, 3, 1]), '0');
+console.log(gimme([5, 10, 14]), '1');
